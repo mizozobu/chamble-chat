42a5766427545bbd3be3516527d654ac1c9828d7
mizozobu <hamc.m.so.j12@gmail.com>
2020-04-28 00:32:52 +0900

    app service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Logger } from '@nestjs/common';
+import { MathService } from './math.service';
+// import { MessagePattern } from '@nestjs/microservices'; <-- Change this
+import { GrpcMethod } from '@nestjs/microservices'; //     <-- to this
+
+interface INumberArray { //      <--
+  data: number[]; //             <--   Add these
+} //                             <--   two
+interface ISumOfNumberArray { // <--   interfaces
+  sum: number; //                <--
+} //                             <--
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger('AppController');
 
-  @Get()
-  getHello(): any {
-    return this.appService.getHello();
-  }
+  constructor(private mathService: MathService) {}
+
+  // @MessagePattern('add')                     <--  Change this
+  @GrpcMethod('AppController', 'Accumulate') // <--  to this
+  //
+  // async accumulate(data: number[])  {              <--  Change method param type
+  //   this.logger.log('Adding ' + data.toString());  <--  and return type to match
+  //   return this.mathService.accumulate(data);      <--  .proto file
+  // }                                                <--
+  accumulate(numberArray: INumberArray, metadata: any): ISumOfNumberArray { // <--
+    this.logger.log('Adding ' + numberArray.data.toString()); //               <--  Should look
+    return { sum: this.mathService.accumulate(numberArray.data) }; //          <--  like this
+  } //                                                                         <--
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const logger = new Logger('Main');
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    url: 'localhost:5000',
+    package: 'app',
+    protoPath: join(__dirname, '../src/app.proto'),
+  },
+};
+
+!async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  app.listen(() => {
+    logger.log('Microservice is listening...');
+  });
+}()
